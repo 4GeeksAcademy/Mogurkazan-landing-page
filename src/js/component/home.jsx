d97bde6c4f3dc5984c51c143dcacf80227efff43
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-03-24 16:19:29 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar";
 import Minicard from "./cards";
 import Jumbotron from "./jumbotron";
 import Footer from "./footer";
 
-//create your first component
+
 const Home = () => {
 	return (
 		<>
@@ -15,7 +13,10 @@ const Home = () => {
 			<div class="container">
 					<Jumbotron/>
 					<div className="row text-center">
-						<Minicard/><Minicard/><Minicard/><Minicard/>
+						<Minicard/>
+						<Minicard/>
+						<Minicard/>
+						<Minicard/>
 					</div>	
 			</div>
 				
